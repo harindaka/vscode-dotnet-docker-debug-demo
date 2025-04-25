31c6aee8b4e435d39e136ae499a08398c21fe2a5
Hemal Athulathmudali <harindaka@gmail.com>
2025-04-25 14:04:41 +1200

    cache nuget

--- .vscode/solution-build.bat
+++ .vscode/solution-build.bat
@@ -8,5 +8,6 @@ docker stop "%CONTAINER_NAME%" >nul 2>&1
 docker run --name "%CONTAINER_NAME%" ^
     -it --rm ^
     -v "%SOLUTION_PATH%":/src ^
+    -v "%SOLUTION_PATH%/.docker/nuget/packages":/root/.nuget/packages ^
     "%IMAGE_NAME%" ^
     dotnet build -c Debug /src
